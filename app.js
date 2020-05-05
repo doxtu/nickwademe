590b1f0fb9ca79d3e65c584d05cda63a9db7a20e
Nick <nickwgonzales@gmail.com>
2020-05-05 18:01:48 -0500

    configure for https

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const http = require('http').createServer(app);
+const https = require('https').createServer({
+   key: fs.readFileSync('/etc/letsencrypt/live/doxtu.me/privkey.pem'),
+   cert: fs.readFileSync('/etc/letsencrypt/live/doxtu.me/fullchain.pem')
+}, app);
 const path = require('path');
-const io = require('socket.io')(http);
-const port = 8080;
+const io = require('socket.io')(https);
+const port = 80;
+const httpsPort = 443;
 const platychat = require('./src/platychat.js');
 
 platychat(io);
@@ -11,3 +17,4 @@ platychat(io);
 app.use(express.static('./public'));
 
 http.listen(port);
+https.listen(httpsPort);
