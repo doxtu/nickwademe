7949385c0e7697fbdbcce5aa0c6e5c4eda3f9c2f
Nick Gonzales <nickwgonzales@gmail.com>
2020-12-09 16:47:30 -0600

    added socket handler code for gallery request, updating build for platychat

--- src/uploader/uploader.js
+++ src/uploader/uploader.js
@@ -9,29 +9,32 @@ module.exports = function (siofu, socket, db) {
   }
 
   async function updateMessageWithFile(e) {
-    const fileIdentifier = '!!upload ' + e.file.meta + '%'
-    const name = e.file.name
+    try {
+      const fileIdentifier = '!!upload ' + e.file.meta + '%'
+      const name = e.file.name
 
-    const fileTag = `images/${name}`
+      const fileTag = `images/${name}`
 
-    await new Promise(function (s, f) {
-      db.all(
-        `
-              UPDATE messages SET rawtext =:filetag WHERE rawtext LIKE :fileIdentifier
-           `,
-        fileTag,
-        fileIdentifier,
-        function (err, rows) {
-          if (err) f(err)
-          s(rows)
-        }
-      )
-    }).catch(console.error)
+      await new Promise(function (s, f) {
+        db.all(
+          `
+            UPDATE messages SET rawtext =:filetag WHERE rawtext LIKE :fileIdentifier
+          `,
+          fileTag,
+          fileIdentifier,
+          function (err, rows) {
+            if (err) f(err)
+            s(rows)
+          }
+        )
+      })
+    } catch (err) {}
   }
 
   const uploader = new siofu()
   uploader.dir = 'public/platychat/images'
   uploader.on('start', updateFileMeta)
   uploader.on('saved', updateMessageWithFile)
+  uploader.on('error', console.error)
   uploader.listen(socket)
 }
