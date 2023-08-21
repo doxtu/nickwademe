a00bac0624bbf752d83fc748d29f567fb3bf842c
Nick <nickwgonzales@gmail.com>
2023-08-21 18:04:18 -0500

    added gallery example and kim-kidney

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/gallery')
+def gallery_page():
+    return render_template('portfolio/index.html')
+
+@app.route('/kim-kidney')
+def kim_kidney():
+    return render_template('kim-kidney/index.html')
